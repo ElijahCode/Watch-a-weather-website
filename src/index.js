e57f863f03602e61d949d372875e80942d2f8e19
Ilya <ilyastarickov1996@gmail.com>
2021-02-07 11:12:02 +0400

    Add new functionaluty.User can watch weather in any city.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,50 @@ function createParagraph(elem, data) {
   const p = document.createElement('p');
 
   p.innerText = data;
+  if (!elem.hasChildNodes()) {
+    elem.appendChild(p);
+  } else {
+    elem.append(p);
+  }
+}
 
-  elem.append(p);
+function createInput(elem) {
+  const input = document.createElement('input');
+
+  input.type = 'text';
+  input.id = 'input';
+
+  elem.append(input);
+}
+
+function createButton(elem) {
+  const button = document.createElement('button');
+
+  button.innerHTML = `
+  <button onclick = 'buttonClick()'>
+  Show weather
+  </button>
+  `;
+
+  elem.append(button);
+}
+
+function createBlock(elem, id, flag = 'app') {
+  const div = document.createElement('div');
+
+  div.id = id;
+
+  if (flag === 'app') {
+    elem.append(div);
+  } else if (flag === 'prep') {
+    elem.prepend(div);
+  }
 }
 
 async function getWeather(cityName) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=29322a2e1ecf125b380cfcee03239f35&units=metric`;
-  const response = await fetch(url);
 
+  const response = await fetch(url);
   const jsonData = await response.json();
 
   return jsonData;
@@ -19,7 +55,6 @@ async function defineUserCity() {
   const url = 'https://get.geojs.io/v1/ip/geo.json';
 
   const response = await fetch(url);
-
   const jsonData = await response.json();
 
   return jsonData.city;
@@ -31,15 +66,34 @@ function createData(inputData) {
     ${inputData.weather[0].main},
     Temperature: ${inputData.main.temp} C,
     Temperature is feels like: ${inputData.main.feels_like} C,
-    Humidity:${inputData.main.humidity},
+    Humidity:${inputData.main.humidity}%,
     Atmospheric pressure: ${inputData.main.pressure} Pa,
     Wind speed: ${inputData.wind.speed} m/s`;
 
   return data;
 }
 
-// draw elemets and show weather in user city
+function getInputText(elemId) {
+  const text = document.getElementById(elemId).value;
+  return text;
+}
 
+function rewriteParagraph(elem, data, position = 0) {
+  const parag = elem.getElementsByTagName('p').item(position);
+  parag.innerText = data;
+}
+
+export default async function buttonClick() {
+  const cityName = getInputText('input');
+
+  const weather = await getWeather(cityName);
+
+  const data = createData(weather);
+
+  rewriteParagraph(document.getElementById('block3'), data);
+}
+
+// draw elemets and show weather in user city
 (async function () {
   const userCity = await defineUserCity();
 
@@ -47,5 +101,16 @@ function createData(inputData) {
 
   const userCityData = createData(userCityWeather);
 
-  createParagraph(document.getElementById('app'), userCityData);
+  createBlock(document.getElementById('app'), 'block1', 'prep');
+  createParagraph(document.getElementById('block1'), userCityData);
 }());
+
+// draw elements
+createBlock(document.getElementById('app'), 'block2');
+
+createParagraph(document.getElementById('block2'), 'Enter a city name:');
+createInput(document.getElementById('block2'));
+createButton(document.getElementById('block2'));
+
+createBlock(document.getElementById('app'), 'block3');
+createParagraph(document.getElementById('block3'), '');
